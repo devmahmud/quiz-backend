bd6c821272927bb3733f13ca1932b2c2e2368839
Mahmudul Alam <expelmahmud@gmail.com>
2021-05-06 13:44:02 +0600

    add: password reset email signal

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.db.models import Q
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class UserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
