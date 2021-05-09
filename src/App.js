ec2bcaec23ecfcd2c23555911db34f7f408bccf3
Mahmudul Alam <expelmahmud@gmail.com>
2021-05-09 17:46:53 +0600

    add: emotion css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from "@emotion/react";
+
 import { Button } from "react-bootstrap";
 import "./app.css";
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="primary">Hello Bootstrap</Button>
+    <div className="App" css={styles}>
+      <Button variant="primary" className="my">
+        Hello Bootstrap
+      </Button>
     </div>
   );
 }
+
+const styles = css`
+  button {
+    background-color: red;
+  }
+`;
 
 export default App;
